8cf2b7ec8eb812333a51166e5d65c2db0f52bb2a
realprinceviper <idrisadeniji01@gmail.com>
2019-10-12 01:45:39 -0400

    set up recipe model and test with sample recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('Pizza', 'how to make pepperoni pizza', 'https://assets3.thrillist.com/v1/image/2808392/size/tmg-article_default_mobile.jpg'),
+    new Recipe('Shawarma', 'how to make chicken shawarma', 'https://i2.wp.com/www.foodrepublic.com/wp-content/uploads/2018/03/shawarma.jpg')
+  ];
 
   constructor() { }
 
